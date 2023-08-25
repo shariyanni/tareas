f1955df8f321186a09f537dac92812c50cad6cae
shari pinto <Sharon_Pinto>
2023-08-25 16:32:47 -0400

    version 1.1

--- post.php
+++ post.php
@@ -11,20 +11,47 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $pais = $_POST['pais']; 
     $correo = $_POST['Correo'];
     $carrera = $_POST['carrera']; 
-    $successMsg = "------------- INSCRIPCIÓN LISTA -------------<br><br>Datos: <br><br> 
-    Nombres: $name <br><br> 
-    Apellido Paterno: $ap_P <br><br> 
-    Apellido Materno: $ap_M <br><br>
-    Dirección: $direc <br><br>
-    Fecha de Nacimiento: $nacimiento <br><br>
-    Teléfono Fijo: $telf <br><br>
-    Teléfono Móvil: $cel <br><br>
-    Lugar de nacimiento:  $lugarNac <br><br>
-    País: $pais  <br><br>
-    Correo electrónico: $correo <br><br>
-    Carrera: $carrera <br><br>
-    ";
+   
+          //  DATABASE SETTINGS 
+          define("DB_HOST", "localhost");
+          define("DB_NAME", "formulario");
+          define("DB_CHARSET", "utf8mb4");
+          define("DB_USER", "root");
+          define("DB_PASSWORD", "");
+          define("DB_PORT", 3307);
+       
+          //  CONNECT TO DATABASE
+          $pdo = new PDO(
+            "mysql:host=".DB_HOST.";port=".DB_PORT.";dbname=".DB_NAME.";charset=".DB_CHARSET, 
+            DB_USER, DB_PASSWORD, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+       
+          if ($pdo) {
+            //INSERT
+            $sql = "INSERT INTO datos (nombre,Apellido_P,Apellido_M,Direccion,fechaNac,Telefono,Celular,lugarNac,pais,email,carrera) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
+            $stmt= $pdo->prepare($sql);
+            $stmt->execute([$name,$ap_P,$ap_M,$direc,$nacimiento,$telf,$cel,$lugarNac,$pais,$correo,$carrera]);
+            echo "<br><br><br><br>Inserción exitosa!<br><br><br><br>";
+            $successMsg = "------------- INSCRIPCIÓN LISTA -------------<br><br>Datos: <br><br> 
+            Nombres: $name <br><br> 
+            Apellido Paterno: $ap_P <br><br> 
+            Apellido Materno: $ap_M <br><br>
+            Dirección: $direc <br><br>
+            Fecha de Nacimiento: $nacimiento <br><br>
+            Teléfono Fijo: $telf <br><br>
+            Teléfono Móvil: $cel <br><br>
+            Lugar de nacimiento:  $lugarNac <br><br>
+            País: $pais  <br><br>
+            Correo electrónico: $correo <br><br>
+            Carrera: $carrera <br><br>
+            ";
+          }
+
+        
 }
+
 ?>
 
 <!DOCTYPE html>
