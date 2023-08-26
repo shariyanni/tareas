6fe7117eef4e86d6b089f452c899fa828fca49c6
shari pinto <Sharon_Pinto>
2023-08-26 07:31:09 -0400

    version 1.2

--- post.php
+++ post.php
@@ -27,14 +27,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                 PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
             ]);
+            // Consulta para obtener los datos de la base de datos
+            $sql = "SELECT * FROM datos";
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute();
+            $datos = $stmt->fetchAll(PDO::FETCH_ASSOC);
        
           if ($pdo) {
             //INSERT
             $sql = "INSERT INTO datos (nombre,Apellido_P,Apellido_M,Direccion,fechaNac,Telefono,Celular,lugarNac,pais,email,carrera) VALUES (?,?,?,?,?,?,?,?,?,?,?)";
             $stmt= $pdo->prepare($sql);
             $stmt->execute([$name,$ap_P,$ap_M,$direc,$nacimiento,$telf,$cel,$lugarNac,$pais,$correo,$carrera]);
-            echo "<br><br><br><br>Inserción exitosa!<br><br><br><br>";
-            $successMsg = "------------- INSCRIPCIÓN LISTA -------------<br><br>Datos: <br><br> 
+            $successMsg = "<br><br>------------- INSCRIPCIÓN LISTA -------------<br><br>Datos: <br><br> 
             Nombres: $name <br><br> 
             Apellido Paterno: $ap_P <br><br> 
             Apellido Materno: $ap_M <br><br>
@@ -57,23 +61,114 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <meta charset="UTF-8">
+<meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tarjeta de Presentación</title>
     <style>
+          body {
+            background-color: #F7D7E8; /* Color de fondo rosado */
+            margin: 0;
+            padding: 0;
+            font-family: Arial, sans-serif;
+        }
+        
         .center-content {
             text-align: center;
-            margin: 0 auto;
-            max-width: 600px;
+            margin: 20px auto;
+            max-width: 800px;
             padding: 20px;
             border: 1px solid #ccc;
+            background-color: #E6C1DE; /* Color de fondo rosado claro */
+            box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1); /* Sombra suave */
+            border-radius: 10px;
+        }
+        
+        h2 {
+            color: #8A2BE2; /* Color morado intenso */
+            margin-top: 0;
+            padding-top: 10px;
+        }
+        
+        .data-table {
+            margin: 20px;
+            padding: 20px;
+            border: 1px solid #ccc;
+            background-color: #B0E0E6; /* Color azul claro */
+            border-radius: 10px;
+        }
+        
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+        
+        th, td {
+            border: 1px solid #ccc;
+            padding: 8px;
+            text-align: left;
+        }
+        
+        th {
+            background-color: #FFB6C1; /* Color rosado claro */
+        }
+        
+        td {
+            background-color: #B0E0E6; /* Color azul claro */
+        }
+        
+        p {
+            color: #6A1B9A; /* Color morado oscuro */
+            font-weight: bold;
         }
     </style>
 </head>
 <body>
-    <div class="center-content">
+<div class="center-content">
         <?php
             echo "<p>$successMsg</p>";
+        ?>
+    </div>
+    <div class="data-table">
+        <?php
+        // Mostrar la tabla con los datos
+        if (!empty($datos)) {
+            echo "<h2>Datos Anteriores</h2>";
+            echo "<table>";
+            echo "<tr>";
+            echo "<th>Nombres</th>";
+            echo "<th>Apellido Paterno</th>";
+            echo "<th>Apellido Materno</th>";
+            echo "<th>Dirección</th>";
+            echo "<th>Fecha de Nacimiento</th>";
+            echo "<th>Teléfono Fijo</th>";
+            echo "<th>Teléfono Móvil</th>";
+            echo "<th>Lugar de Nacimiento</th>";
+            echo "<th>País</th>";
+            echo "<th>Correo Electrónico</th>";
+            echo "<th>Carrera</th>";
+            echo "</tr>";
+
+            foreach ($datos as $dato) {
+                echo "<tr>";
+                echo "<td>{$dato['nombre']}</td>";
+                echo "<td>{$dato['Apellido_P']}</td>";
+                echo "<td>{$dato['Apellido_M']}</td>";
+                echo "<td>{$dato['Direccion']}</td>";
+                echo "<td>{$dato['fechaNac']}</td>";
+                echo "<td>{$dato['Telefono']}</td>";
+                echo "<td>{$dato['Celular']}</td>";
+                echo "<td>{$dato['lugarNac']}</td>";
+                echo "<td>{$dato['pais']}</td>";
+                echo "<td>{$dato['email']}</td>";
+                echo "<td>{$dato['carrera']}</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+        } else {
+            echo "<p>No hay datos disponibles.</p>";
+        }
         ?>
     </div>
 </body>
